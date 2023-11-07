6bffcb306ecd90053b7914b2b3ca57b90441b3c4
Андрей Скляр <andreas.skliar@gmail.com>
2023-11-07 11:40:44 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,59 @@
-import { Component } from "react"
+import React, { useState } from "react";
 import { countTotalFeedback } from "./Statistics/countTotalFeedback";
 import { countPositiveFeedbackPercentage } from "./Statistics/countPositiveFeedbackPercentage";
 import { Section } from "./Section/Section";
 import { FeedbackOptions } from "./Feedback/FeedbackOptions";
 import { Statistics } from "./Statistics/Statistics";
-import { Notification } from "./Notificati0n/Notification";
 import friends from './data/friends.json';
 import { Gallery } from "./Gallery/Gallery";
+import { Notification } from "./Notificati0n/Notification";
 
-export class App extends Component {
-  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+export const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleOption = option => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1
+    }));
   }
 
-  handleOption = (option) => {
-    this.setState((prevState) => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-  });}
+  const totalFeedback = countTotalFeedback(feedback);
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = countTotalFeedback(this.state);
-    const statisticsData = [
-      { title: "Good", value: good },
-      { title: "Neutral", value: neutral },
-      { title: "Bad", value: bad },
-      { title: "Total", value: totalFeedback },
-      { title: "Positive feedback", value: countPositiveFeedbackPercentage(this.state) },
-    ];
+  const statisticsData = [
+    { title: "Good", value: feedback.good },
+    { title: "Neutral", value: feedback.neutral },
+    { title: "Bad", value: feedback.bad },
+    { title: "Total", value: totalFeedback },
+    { title: "Positive feedback", value: countPositiveFeedbackPercentage(feedback) },
+  ];
 
-    return (
-      <div className="container">
-        <div className="wrapper">
-          <div className="wrapper__header">
-            <Section title="Please leave feedback">
-              <FeedbackOptions
-                onLeaveFeedback={this.handleOption}
-                options={Object.keys(this.state)}
-                friends={friends}
-              />
-            </Section>
-            <Section title="Statistics">
-              {totalFeedback > 0
-                ? (<Statistics data={statisticsData} />)
-                : (<Notification message="There is no feedback" />)
-              }
-            </Section>
-          </div>
-          <Gallery friends={friends} />
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="wrapper__header">
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              onLeaveFeedback={handleOption}
+              options={Object.keys(feedback)}
+              friends={friends}
+            />
+          </Section>
+          <Section title="Statistics">
+            {totalFeedback > 0
+              ? (<Statistics data={statisticsData} />)
+              : (<Notification message="There is no feedback" />)
+            }
+          </Section>
         </div>
+        <Gallery friends={friends} />
       </div>
-    );
-  }
-};
+    </div>
+  );
+}
+
+
